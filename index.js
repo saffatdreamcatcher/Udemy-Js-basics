39867aae4a020387c6fa85bc8605c50766b6d5dd
Saffat Ahmed <s.ahmed@siderian.com>
2024-01-08 13:57:43 +0600

    control flow in progress

---
+++ index.js
@@ -0,0 +1,153 @@
+// Object (RefType)
+let person = {name:'Mosh',
+            age:30
+          };
+
+          //Dot Notation
+          person.age =40;
+          console.log(person.age);
+    
+// ternary operator
+let points= 110;
+let type= points > 100 ? 'gold' : 'silver';      
+console.log(type);    
+
+let hour = 11;
+if (hour >= 6 && hour < 10)
+console.log ('morning');
+else if (hour==12 || hour==24);
+else(console.log('something else'));
+
+//switch case
+let role = 'guest';
+switch (role){
+case 'admin':
+console.log('Admin User');
+//break;
+
+case 'guest':
+console.log('Guest User');
+break;
+
+default:
+  console.log('Unknown User');
+}
+
+//for loop
+for (i=0; i<=5; i++)
+{
+  console.log('Welcome To JS');
+}
+
+for (let i = 0; i <=5; i++) {
+if (i%2 ==0) console.log(i);
+}
+
+//while
+// let i = 0;
+// while (i<=5) {
+// if (i%2==0) console.log(i);
+// i++;
+// }
+
+//do-while
+let i = 0;
+do {
+  if (i%2==0) console.log(i);
+  i++;
+} while (i<=5);
+
+//infinite-loop
+let j = 0;
+while (j<5) {
+  console.log(j);
+}
+
+//for-in
+const person = {
+  name:'Mosh',
+  age: 30
+};
+for (let key in person)
+console.log(key, person[key]);
+
+const colors = ['red', 'green', 'blue'];
+// for ( let index in colors)
+// console.log(index, colors[index]);
+
+//for-of
+for (let color of colors)
+console.log(color);
+
+// break and continue
+
+let k = 0;
+while (k <=5) {
+// if (k==3) break;
+if (k % 2 == 0) {
+  k++;
+  continue;
+}
+
+console.log(k);
+k++;
+}
+
+//Ex
+let number = max(6,3);
+console.log(number);  
+
+function max(a, b) {
+  // if (a> b) return a;
+  // return b;
+  return (a>b) ? a: b;
+}
+
+//11-Ex
+console.log(isLandscape(800, 600));
+function isLandscape(width, height) 
+{
+  // return(width > height ? true: false);
+  return (width > height);
+}
+
+//11-Ex fizzBuzz
+const output = fizzBuzz(14);
+console.log(output);
+
+function fizzBuzz(input) {
+  for (let m=1; m<=16; m++)
+  {
+    if (m % 3 == 0 && m % 5 == 0) {
+      console.log("FizzBuzz")
+    }
+    else if ( m % 3 == 0) {
+      console.log("Fizz")
+    }
+    else if ( m % 5 == 0) {
+      console.log("Buzz")
+    }
+    else {
+      console.log(m)
+    }
+  }
+}
+
+//12-Ex demerit points
+checkSpeed(130);
+
+function checkSpeed(speed) {
+  const speedLimit = 70;
+  const kmPerPoint = 5
+
+  if (speed < speedLimit + kmPerPoint)
+  console.log('Ok');
+else {
+  const points = Math.floor((speed - speedLimit) / kmPerPoint);
+  if (points >=12)
+  console.log('License suspended');
+else
+console.log('Points', points)
+}
+}
+
