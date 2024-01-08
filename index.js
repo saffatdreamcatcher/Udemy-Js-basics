ccef74f5c2ada255ed48fc8a80428c049177fa0a
Saffat Ahmed <s.ahmed@siderian.com>
2024-01-08 17:12:22 +0600

    18 -Exercise done!

--- index.js
+++ index.js
@@ -178,7 +178,7 @@ function showProperties(obj) {
  console.log(key, obj[key]);
 }
 
-//17- Sum of Multiple
+//17- Ex Sum of Multiple
 
 console.log(sum(10));
 
@@ -190,4 +190,33 @@ function sum(limit) {
   sum += i;
 
 return sum;
+}
+
+//18- Ex Grade
+
+const marks = [80, 80, 50];
+console.log(calculateGrade(array));
+
+function calculateGrade(marks) {
+//   let sum = 0;
+//   for (let mark of marks)
+//   sum += mark;
+// let average = sum / marks.length;
+
+const average = calculateAverage(marks);
+
+if (average < 60) return 'Grade F';
+if (average < 70) return 'Grade D';
+if (average < 80) return 'Grade C';
+if (average < 90) return 'Grade B';
+return 'Grade A';
+}
+
+function calculateAverage(array) {
+  let sum = 0;
+  for (let value of array)
+  sum += value;
+// let average = sum / array.length;
+// return average;
+return sum / array.length;
 }
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,11 +1,26 @@
-console.log(sum(10));
+const marks = [80, 80, 50];
+console.log(calculateGrade(array));
 
-function sum(limit) {
+function calculateGrade(marks) {
+//   let sum = 0;
+//   for (let mark of marks)
+//   sum += mark;
+// let average = sum / marks.length;
+
+const average = calculateAverage(marks);
+
+if (average < 60) return 'Grade F';
+if (average < 70) return 'Grade D';
+if (average < 80) return 'Grade C';
+if (average < 90) return 'Grade B';
+return 'Grade A';
+}
+
+function calculateAverage(array) {
   let sum = 0;
-
-  for (let i=0; i<= limit; i++)
-  if (i % 3 === 0 ||i % 5 === 0 )
-  sum += i;
-
-return sum;
+  for (let value of array)
+  sum += value;
+// let average = sum / array.length;
+// return average;
+return sum / array.length;
 }
