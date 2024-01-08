5e6fee32d53ef00f30cb771ccac94a8761dfc363
Saffat Ahmed <s.ahmed@siderian.com>
2024-01-08 20:40:09 +0600

    Ex 3 done!

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,8 +1,23 @@
-let address3 = new Address2('St carolina', 'new york', 5768)
-console.log(address3)
+let address11 = new Address('St carolina', 'new york', 5768);
+let address12 = new Address('St carolina', 'new york', 5768);
+let address13 = address11;
 
-function Address2(street, city, zipCode) {
-this.street = street;
-this.city = city;
-this.zipCode = zipCode;
-}
+console.log(areEqual(address11, address12));
+console.log(areSame(address11, address12));
+console.log(areSame(address11, address13));
+
+function Address(street, city, zipCode) {
+  this.street = street;
+  this.city = city;
+  this.zipCode = zipCode;
+  }
+
+  function areEqual(address11, address12) {
+   return address11.street === address12.street && 
+   address11.city === address12.city && 
+   address11.zipCode === address12.zipCode;
+  }
+
+  function areSame(address11, address12) {
+    return address11 === address12;
+  }
