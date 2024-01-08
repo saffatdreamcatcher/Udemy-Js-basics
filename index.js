cf8f472d891d93db4dd27d774de2f5ac81d4afc0
Saffat Ahmed <s.ahmed@siderian.com>
2024-01-08 15:19:10 +0600

    16- Exercise done!

--- index.js
+++ index.js
@@ -162,3 +162,18 @@ function countTruthy(array) {
 return count;
 }
 
+//16- Ex String Properties
+const movie = {
+  title: 'a',
+  releaseYear: 2018,
+  rating: 4.5,
+  director: 'b'
+};
+showProperties(movie);
+
+function showProperties(obj) {
+  for (let key in obj)
+  // console.log(key);
+ if (typeof obj[key] == 'string')
+ console.log(key, obj[key]);
+}
