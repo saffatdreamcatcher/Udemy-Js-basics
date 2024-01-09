bf2e9dff4751abf7504be62898df8ec6582d3f8a
Saffat Ahmed <s.ahmed@siderian.com>
2024-01-09 16:36:49 +0600

    94.4 done!

--- index.js
+++ index.js
@@ -707,3 +707,15 @@ const titles = movies
 
   console.log(sum(1, 2, 3, 4, 5));
 
+  //the rest Operator
+
+  function sum(discount, ...prices) {
+    //console.log(args);
+    // return args.reduce((a,b) => a + b);
+    const total = prices.reduce((a,b) => a + b);
+    return total * (1- discount);
+  }
+
+  // console.log(sum(1, 2, 3, 4, 5, 10));
+  console.log(sum(0.1, 20, 30));
+
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,11 +1,13 @@
-  function sum(a, b) {
+  console.log(sum(1, 2, 3, 4, 5));
 
-    let total = 0;
-    for (let value of arguments)
-    total += value;
-  return total;
-    console.log(arguments);
-    return a + b;    // 1 + undefined
+  //the rest Operator
+
+  function sum(discount, ...prices) {
+    //console.log(args);
+    // return args.reduce((a,b) => a + b);
+    const total = prices.reduce((a,b) => a + b);
+    return total * (1- discount);
   }
 
-  console.log(sum(1, 2, 3, 4, 5));
+  // console.log(sum(1, 2, 3, 4, 5, 10));
+  console.log(sum(0.1, 20, 30));
