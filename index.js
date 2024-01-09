fd5bee67eee9fc5e76a04e0c38d1966479c3c9bf
Saffat Ahmed <s.ahmed@siderian.com>
2024-01-09 14:25:32 +0600

    86.19 Exercise 3 done!

--- index.js
+++ index.js
@@ -569,3 +569,19 @@ function includes(array, searchElement) {
   return true;
 return false;
 }
+
+//Exercise 3 Except
+
+const nms2 = [1, 2, 3, 4, 1, 1, 1];
+
+const output2 = except(nms2, [1, 2]);
+
+console.log(output2);
+
+function except(array, excluded) {
+  const output2 = [];
+  for (let element of array)
+  if (!excluded.includes(element))
+  output2.push(element);
+return output2;
+}
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,10 +1,14 @@
-const nms = [1, 2, 3, 4];
 
-console.log(nms.includes(5));
+const nms2 = [1, 2, 3, 4, 1, 1, 1];
 
-function includes(array, searchElement) {
+const output2 = except(nms2, [1, 2]);
+
+console.log(output2);
+
+function except(array, excluded) {
+  const output2 = [];
   for (let element of array)
-  if(element === searchElement)
-  return true;
-return false;
+  if (!excluded.includes(element))
+  output2.push(element);
+return output2;
 }
