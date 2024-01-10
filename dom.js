c7f4d528d5e31a1fdd299e5d7c3feeab0e2c4cc6
Saffat Ahmed <s.ahmed@siderian.com>
2024-01-10 19:04:38 +0600

    dom.js modified!

--- dom.js
+++ dom.js
@@ -3,7 +3,8 @@ const link = document.querySelector("a");
 link.textContent = "Mozilla Developer Network for beginner";
 
 const sect = document.querySelector("section");
-const para = document.createElement("h3");
+const para = document.createElement("p");
 para.textContent = "We hope you enjoyed the ride.";
+sect.appendChild(para);
 
 // link.href = "https://developer.mozilla.org";
