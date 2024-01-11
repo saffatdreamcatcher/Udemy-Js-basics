31eade370e9b80f67a4641130f824facabb8abdc
Saffat Ahmed <s.ahmed@siderian.com>
2024-01-11 13:49:10 +0600

    tried different approach (using different loop) to displayData!

--- api-fetch.js
+++ api-fetch.js
@@ -5,24 +5,42 @@ function loadApiData() {
 }
 
 function displayData(data) {
-  const div = document.getElementById('divTag');
-  data.map(user => {
-    const p = document.createElement('p');
-    p.innerText = user.title;
-    div.appendChild(p);
-  });
-  
 
   // const div = document.getElementById('divTag');
-  
-  // for (let i = 0; i <= data.length; i++)
-  // {
-  //   console.log('div:', data[i]);
-  // }
-
+  // data.map(user => {
   //   const p = document.createElement('p');
   //   p.innerText = user.title;
   //   div.appendChild(p);
+  // });
+
+
+  
+  //Using for loop
+
+  const div = document.getElementById('divTag');
+  
+  for (let i = 0; i <= data.length; i++)
+  {
+    console.log('div:', data[i]);
+
+    const p = document.createElement('p');
+    p.innerText = data[i].title;
+    div.appendChild(p);
+  }
+
+   
  
+
+  //Using forEach loop
+
+  // const div = document.getElementById('divTag');
+  
+  // data.forEach ( para => {
+  //   console.log('div:', para);
+  //   const p = document.createElement('p');
+  //   p.innerText = para.title;
+  //   div.appendChild(p);
+  // });
+
 }
 
