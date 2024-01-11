8405eb8ebf267af77846eb84d23d7adbb01bf81d
Saffat Ahmed <s.ahmed@siderian.com>
2024-01-11 14:49:02 +0600

    setTimeout added in loadApiData()!

--- api-fetch.js
+++ api-fetch.js
@@ -1,32 +1,34 @@
 function loadApiData() {
+  setTimeout(() => {
   fetch('https://jsonplaceholder.typicode.com/posts')
 .then(response => response.json())
 .then(data => displayData(data)) 
+}, 3000 );
 }
 
 function displayData(data) {
 
-  // const div = document.getElementById('divTag');
-  // data.map(user => {
-  //   const p = document.createElement('p');
-  //   p.innerText = user.title;
-  //   div.appendChild(p);
-  // });
+  const div = document.getElementById('divTag');
+  data.map(user => {
+    const p = document.createElement('p');
+    p.innerText = user.title;
+    div.appendChild(p);
+  });
 
 
   
   //Using for loop
 
-  const div = document.getElementById('divTag');
+  // const div = document.getElementById('divTag');
   
-  for (let i = 0; i <= data.length; i++)
-  {
-    console.log('div:', data[i]);
+  // for (let i = 0; i <= data.length; i++)
+  // {
+  //   console.log('div:', data[i]);
 
-    const p = document.createElement('p');
-    p.innerText = data[i].title;
-    div.appendChild(p);
-  }
+  //   const p = document.createElement('p');
+  //   p.innerText = data[i].title;
+  //   div.appendChild(p);
+  // }
 
    
  
