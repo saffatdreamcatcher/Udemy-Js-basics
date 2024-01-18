c701d45c17b8381705e091a4ceede1ddc08b9fbd
Saffat Ahmed <s.ahmed@siderian.com>
2024-01-18 18:31:15 +0600

    Firebase realtime database linked to the project!

--- MobileApp/index.js
+++ MobileApp/index.js
@@ -1,13 +1,59 @@
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"  
+// import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+// const appSettings = {
+//   databaseURL: "https://playground-d52a0-default-rtdb.asia-southeast1.firebasedatabase.app/"
+// }
+
+// const app = initializeApp(appSettings);
+// const database = getDatabase(app)
+// const moviesInDB = ref(database, "movies")
+
+// const inputFieldEl = document.getElementById("input-field");
+// const addButtonEl = document.getElementById("add-button");
+
+// addButtonEl.addEventListener("click", function() {
+//   let inputValue = inputFieldEl.value;
+
+//   push(moviesInDB, inputValue);
+
+//   if (inputValue === "") {
+//     console.log("Input is blank. Please enter a value.");
+//   } else {
+//     // console.log("Input value: ", inputValue);
+//     console.log(`${inputValue} added to database`)
+  
+//   }
+
+// })
+
+
+// Firebase data implementation
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"  
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+  databaseURL: "https://playground-d52a0-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app)
+const moviesInDB = ref(database, "movies")
+
 const inputFieldEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
 
 addButtonEl.addEventListener("click", function() {
   let inputValue = inputFieldEl.value;
 
+  push(moviesInDB, inputValue);
+
   if (inputValue === "") {
     console.log("Input is blank. Please enter a value.");
   } else {
-    console.log("Input value: ", inputValue);
+    // console.log("Input value: ", inputValue);
+    console.log(`${inputValue} added to database`)
   
   }
 
