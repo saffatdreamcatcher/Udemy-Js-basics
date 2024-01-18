7a102e5756fd4a031ef6659681d493b9cdc118a1
Saffat Ahmed <s.ahmed@siderian.com>
2024-01-18 13:57:51 +0600

    css added to style.css!

--- MobileApp/index.js
+++ MobileApp/index.js
@@ -3,6 +3,12 @@ const addButtonEl = document.getElementById("add-button");
 
 addButtonEl.addEventListener("click", function() {
   let inputValue = inputFieldEl.value;
+
+  if (inputValue === "") {
+    console.log("Input is blank. Please enter a value.");
+  } else {
+    console.log("Input value: ", inputValue);
   
-  console.log(inputValue);
+  }
+
 })
