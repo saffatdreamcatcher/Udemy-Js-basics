04ffcd60d3481917ecc23fff3da7eda371e59bcf
Saffat Ahmed <s.ahmed@siderian.com>
2024-03-04 11:51:01 +0600

    flag and country info done!

--- COUNTRY_INFO/script.js
+++ COUNTRY_INFO/script.js
@@ -4,26 +4,27 @@ const loadCountry = () => {
     .then((countries) => displayCountries(countries));
 };
 const displayCountries = (countries) => {
-  console.log("COUNTRIES:", countries);
+  // console.log("COUNTRIES:", countries);
   const countryGlobal = document.getElementById("country");
   countryGlobal.innerHTML = "";
 
   countries.map((country) => {
-    console.log(country);
+    const languages = country.languages;
+    console.log(languages);
     const div = document.createElement("div");
     div.classList.add("col-lg-4");
     div.classList.add("col-lg-4");
     div.classList.add("col-lg-4");
     div.innerHTML = `<div class="card">
                     <div class="d-flex justify-content-center align-items-center">
-                    <img src="${
-                      country.flag ? country.flags.png : "N/A"
-                    }" width="100%" alt="">
+                    <img src="${country.flags.png}" width="100%" alt="">
                     </div>
                     <h5>${country.name.common}</h5>
                     <h6>Official Name : ${country.name.official}</h6>
                     <h6>Capital : ${country.capital}</h6>
-                    <h6>Language : ${country.languages.tur}</h6>
+                    <h6>Lnaguages: ${Object.keys(languages).map(
+                      (key) => languages[key]
+                    )}</h6>
                     </div>`;
     countryGlobal.appendChild(div);
   });
