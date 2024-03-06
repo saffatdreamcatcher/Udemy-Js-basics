1f4cad4af5dcf56d88d554b49aa6deaaa3982be0
Saffat Ahmed <s.ahmed@siderian.com>
2024-03-06 15:54:17 +0600

    Picture in picture done!

--- Picture-in-picture/script.js
+++ Picture-in-picture/script.js
@@ -1,1 +1,25 @@
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
 
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    console.log("whoops, error here:", error);
+  }
+}
+
+button.addEventListener("click", async () => {
+  //Disable Button
+  button.disabled = true;
+  await videoElement.requestPictureInPicture();
+  // Reset Button
+  button.disabled = false;
+});
+
+// On Load
+selectMediaStream();
