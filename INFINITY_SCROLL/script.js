0ede53a22d1768bac5026538ac12e758ed0bc495
Saffat Ahmed <s.ahmed@siderian.com>
2024-03-12 17:46:11 +0600

    custom countdown in progress!

--- INFINITY_SCROLL/script.js
+++ INFINITY_SCROLL/script.js
@@ -32,25 +32,25 @@ function displayPhotos() {
   imagesLoaded = 0;
   totalImages = photosArray.length;
   photosArray.forEach((photo) => {
-    // Create <a> to link to Unsplash
-    const item = document.createElement("a");
-    setAttributes(item, {
-      href: photo.links.html,
-      target: "_blank",
-    });
-    // Create <img> for photo
-    const img = document.createElement("img");
-    setAttributes(img, {
-      src: photo.urls.regular,
-      alt: photo.alt_description,
-      title: photo.alt_description,
-    });
-    // Event Listener, check when each is finished loading
-    img.addEventListener("load", imageLoaded);
-    // Put <img> inside <a>, then put both inside imageContainer Element
-    item.appendChild(img);
-    imageContainer.appendChild(item);
-  });
+     // Create <a> to link to Unsplash
+     const item = document.createElement("a");
+     setAttributes(item, {
+       href: photo.links.html,
+       target: "_blank",
+     });
+     // Create <img> for photo
+     const img = document.createElement("img");
+     setAttributes(img, {
+       src: photo.urls.regular,
+       alt: photo.alt_description,
+       title: photo.alt_description,
+     });
+     // Event Listener, check when each is finished loading
+     img.addEventListener("load", imageLoaded);
+     // Put <img> inside <a>, then put both inside imageContainer Element
+     item.appendChild(img);
+     imageContainer.appendChild(item);
+   });
 }
 
 // Get photos from Unsplash API
@@ -70,7 +70,7 @@ window.addEventListener("scroll", () => {
     document.body.offsetHeight - 1000
   ) {
     getPhotos();
-    // console.log("load more");
+    console.log("load more");
   }
 });
 
--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 # Js basic
 
-
 - Quote Generator -> https://regal-stardust-0bb8c8.netlify.app/
-- Infinity Scroll -> https://snazzy-wisp-1b2aa9.netlify.app/
+- Infinite Scroll -> https://reliable-begonia-aee35e.netlify.app/
 - Picture in Picture -> https://benevolent-pixie-432e92.netlify.app/
 - Joke Teller -> https://jocular-kheer-c492a7.netlify.app/
-- Light and Dark Mode ->  https://timely-syrniki-3fc612.netlify.app/
+- Light and Dark Mode -> https://timely-syrniki-3fc612.netlify.app/
 - Navigation Nation -> https://gleeful-sfogliatella-d0698d.netlify.app/
 
-
-- Country Flag and Information ->  https://steady-naiad-5626dc.netlify.app/
-- Action Reset Task (DOM manipulation) ->  https://nimble-starburst-51a364.netlify.app/
+- Country Flag and Information -> https://steady-naiad-5626dc.netlify.app/
+- Action Reset Task (DOM manipulation) -> https://nimble-starburst-51a364.netlify.app/
